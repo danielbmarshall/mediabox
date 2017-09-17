6a78d9f557996f655c0c034b5ff8a339cb084779
danielbmarshall <danielbmarshall@users.noreply.github.com>
2017-09-17 18:41:09 -0500

    Add files via upload

---
+++ settings.ini.php
@@ -0,0 +1,174 @@
+; <?php die('Access denied'); ?>
+[general]
+title = "Muximux - Application Management Console"
+branch = "master"
+theme = "classic"
+color = "#31ac63"
+splashscreen = "true"
+tabcolor = "true"
+rss = "true"
+rssUrl = "https://www.wired.com/feed/"
+userNameInput = "admin"
+password = "muximux"
+
+[settings]
+branch_changed = ""
+sha = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+enabled = "true"
+default = "false"
+name = "Settings"
+url = "muximux.php"
+landingpage = "false"
+icon = "fa-cog"
+dd = "true"
+branches[develop] = "f1135f2a0a7d6d69dbcb086bd71d5e8bee6447c3"
+branches[experimental] = "262b1a4c4ca6fa0386e46400211604d66c0bd22d"
+branches[master] = "66b11488206b44364b6ed8bb438462cbbb0835c5"
+branches[parseError] = "14ea3581389e329e27c496367b2b85b15fc106f9"
+branches[saveProblem] = "4d16f3f4b1d7fb18bac5d8ed09b9a45dbe6711ff"
+last_check = 1505691225
+
+[773680newApplication]
+name = "Portainer"
+url = "http://IP_ADDRESS:9000"
+scale = 1
+icon = "muximux-database"
+color = "#bf9000"
+enabled = "true"
+dd = "true"
+
+[Plex]
+name = "Plex"
+url = "http://IP_ADDRESS:32400/web/"
+scale = 1
+icon = "muximux-plex"
+color = "#f9be03"
+enabled = "true"
+dd = "true"
+default = "true"
+
+[PlexPy]
+name = "PlexPy"
+url = "http://IP_ADDRESS:8181"
+scale = 1
+icon = "muximux-plexivity"
+color = "#cc7b19"
+enabled = "true"
+dd = "true"
+
+[Ombi]
+name = "Ombi"
+url = "http://IP_ADDRESS:3579/"
+scale = 1
+icon = "muximux-plex"
+color = "#df691a"
+enabled = "true"
+dd = "true"
+
+[Couchpotato]
+name = "CouchPotato"
+url = "http://IP_ADDRESS:5050"
+scale = 1
+icon = "muximux-couchpotato"
+color = "#f85c22"
+enabled = "true"
+dd = "true"
+
+[Sonarr]
+name = "Sonarr"
+url = "http://IP_ADDRESS:8989/"
+scale = 1
+icon = "muximux-sonarr"
+color = "#35c5f4"
+enabled = "true"
+dd = "true"
+
+[870113newApplication]
+name = "Radarr"
+url = "http://IP_ADDRESS:7878/"
+scale = 1
+icon = "muximux-movie_filter"
+color = "#cc0000"
+enabled = "true"
+dd = "true"
+
+[625037newApplication]
+name = "Sickrage"
+url = "http://IP_ADDRESS:8081/home/"
+scale = 1
+icon = "muximux-play"
+color = "#0c343d"
+enabled = "true"
+dd = "true"
+
+[deluge]
+name = "Deluge VPN"
+url = "http://IP_ADDRESS:8112/"
+scale = 1
+icon = "muximux-deluge"
+color = "#9900ff"
+enabled = "true"
+dd = "true"
+
+[705606newApplication]
+name = "UniFi"
+url = "http://IP_ADDRESS:8443"
+scale = 1
+icon = "muximux-connection"
+color = "#6fa8dc"
+enabled = "true"
+dd = "true"
+
+[812377newApplication]
+name = "Home Assistant"
+url = "http://IP_ADDRESS:8123/"
+scale = 1
+icon = "muximux-home2"
+color = "#674ea7"
+enabled = "true"
+dd = "true"
+
+[678211newApplication]
+name = "HTPC Manager"
+url = "http://IP_ADDRESS:8085/"
+scale = 1
+icon = "muximux-home"
+color = "#e69138"
+enabled = "true"
+dd = "true"
+
+[793005newApplication]
+name = "Ubooquity - Admin"
+url = "http://IP_ADDRESS:2203/ubooquity/admin/"
+scale = 1
+icon = "muximux-book"
+color = "#b6d7a8"
+enabled = "true"
+dd = "true"
+
+[666910newApplication]
+name = "Ubooquity - Library"
+url = "http://IP_ADDRESS:2202/ubooquity/"
+scale = 1
+icon = "muximux-books"
+color = "#ead1dc"
+enabled = "true"
+dd = "true"
+
+[124879newApplication]
+name = "Minio"
+url = "http://IP_ADDRESS:9009/minio/login"
+scale = 1
+icon = "muximux-storage"
+color = "#00ff00"
+enabled = "true"
+dd = "true"
+
+[449278newApplication]
+name = "Setup Instructions"
+url = "http://IP_ADDRESS:8585"
+scale = 1
+icon = "muximux-settings"
+color = ""
+enabled = "true"
+dd = "true"
